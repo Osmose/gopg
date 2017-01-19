ca902b4b053495001ff14b6bafe58d6c5256b389
Michael Kelly <mkelly@mozilla.com>
2017-01-18 23:37:08 -0800

    Ability to calculate groups.

--- gopg.js
+++ gopg.js
@@ -12,18 +12,80 @@ class Board {
     this.cells = [];
 
     for (let row = 0; row < size; row++) {
-      const rowItems = [];
       const rowElem = document.createElement('tr');
       for (let col = 0; col < size; col++) {
         const cell = new Cell(row, col);
-        rowItems.push(cell);
+        this.cells.push(cell);
         rowElem.appendChild(cell.elem);
       }
-      this.cells.push(rowItems);
       this.elem.appendChild(rowElem);
     }
 
     this.elem.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  findGroups() {
+    const groups = [];
+    const checked = new Array(this.size * this.size).fill(false);
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        if (checked[this.index(row, col)]) {
+          continue;
+        }
+
+        const color = this.get(row, col);
+        if (color === EMPTY) {
+          checked[this.index(row, col)] = true;
+          continue;
+        }
+
+        const group = this.findGroupAt(row, col, color, checked);
+        if (group !== null) {
+          groups.push(group);
+        }
+      }
+    }
+    console.log(groups);
+  }
+
+  findGroupAt(row, col, color, checked) {
+    if (checked[this.index(row, col)]) {
+      return null;
+    }
+
+    if (this.get(row, col) !== color) {
+      return null;
+    }
+
+    checked[this.index(row, col)] = true;
+    let group = new Group(color);
+    group.add(this.getCell(row, col));
+    if (row > 0 && this.get(row - 1, col) === color) {
+      const otherGroup = this.findGroupAt(row - 1, col, color, checked);
+      if (otherGroup !== null) {
+        group = group.merge(otherGroup);
+      }
+    }
+    if (row < this.size - 1 && this.get(row + 1, col) === color) {
+      const otherGroup = this.findGroupAt(row + 1, col, color, checked);
+      if (otherGroup !== null) {
+        group = group.merge(otherGroup);
+      }
+    }
+    if (col > 0 && this.get(row, col - 1) === color) {
+      const otherGroup = this.findGroupAt(row, col - 1, color, checked);
+      if (otherGroup !== null) {
+        group = group.merge(otherGroup);
+      }
+    }
+    if (col < this.size - 1 && this.get(row, col + 1) === color) {
+      const otherGroup = this.findGroupAt(row, col + 1, color, checked);
+      if (otherGroup !== null) {
+        group = group.merge(otherGroup);
+      }
+    }
+
+    return group;
   }
 
   handleClick(event) {
@@ -42,13 +104,45 @@ class Board {
     }
   }
 
+  index(row, col) {
+    return (row * this.size) + col;
+  }
+
   set(row, col, value) {
-    this.cells[row][col].value = value;
+    this.getCell(row, col).value = value;
+  }
+
+  get(row, col) {
+    return this.getCell(row, col).value;
+  }
+
+  getCell(row, col) {
+    return this.cells[this.index(row, col)];
+  }
+}
+
+class Group {
+  constructor(color, cells = []) {
+    this.cells = cells;
+    this.color = color;
+  }
+
+  add(cell) {
+    this.cells.push(cell);
+  }
+
+  merge(otherGroup) {
+    if (otherGroup.color !== this.color) {
+      throw new Error('Cannot merge two groups of differing colors.');
+    }
+    return new Group(this.color, this.cells.concat(otherGroup.cells));
   }
 }
 
 class Cell {
   constructor(row, col) {
+    this.row = row;
+    this.col = col;
     this.elem = document.createElement('td');
     this.elem.classList.add('cell');
     this.elem.dataset.row = row;
@@ -76,5 +170,5 @@ class Cell {
   }
 }
 
-const board = new Board(9);
+const board = window.board = new Board(9);
 document.body.appendChild(board.elem);
